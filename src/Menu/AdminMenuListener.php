d33061f2885ac85cb3182f2d5c5df432a4da1817
Robinson Roca <rroca@praga.ws>
2020-02-04 00:19:57 -0600

    Bootstrap 4 added to PWSTheme

---
+++ src/Menu/AdminMenuListener.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Menu;
+
+use Sylius\Bundle\UiBundle\Menu\Event\MenuBuilderEvent;
+
+final class AdminMenuListener
+{
+    public function addAdminMenuItems(MenuBuilderEvent $event): void
+    {
+        $menu = $event->getMenu();
+
+        $newSubmenu = $menu
+            ->addChild('new')
+            ->setLabel('Custom Admin Submenu')
+        ;
+
+        $newSubmenu
+            ->addChild('new-subitem')
+            ->setLabel('Custom Admin Menu Itemu')
+        ;
+    }
+}
