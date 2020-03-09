957bc1a8d149928fd0591b5dc112cfe17d9e26b4
Rodmar Zavala <rodmar.zavala@gmail.com>
2020-03-09 12:13:22 -0600

    Terms and conditions accepted at field to shop user.

--- src/Entity/User/ShopUser.php
+++ src/Entity/User/ShopUser.php
@@ -13,4 +13,25 @@ use Sylius\Component\Core\Model\ShopUser as BaseShopUser;
  */
 class ShopUser extends BaseShopUser
 {
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="terms_and_conditions_accepted_at", type="datetime", nullable=true)
+     */
+    private $termsAndConditionsAcceptedAt;
+
+    /**
+     * @return \DateTime
+     */
+    public function getTermsAndConditionsAcceptedAt(): \DateTime
+    {
+        return $this->termsAndConditionsAcceptedAt;
+    }
+
+    /**
+     * @param \DateTime $termsAndConditionsAcceptedAt
+     */
+    public function setTermsAndConditionsAcceptedAt(\DateTime $termsAndConditionsAcceptedAt): void
+    {
+        $this->termsAndConditionsAcceptedAt = $termsAndConditionsAcceptedAt;
+    }
 }
