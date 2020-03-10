1cb1a7f941989bc8998cb59fd83265f96b3335f7
Rodmar Zavala <rodmar.zavala@gmail.com>
2020-03-09 22:48:48 -0600

    $totalOfDiscounts on coupon show

--- src/Entity/Promotion/PromotionAction.php
+++ src/Entity/Promotion/PromotionAction.php
@@ -5,12 +5,19 @@ declare(strict_types=1);
 namespace App\Entity\Promotion;
 
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 use Sylius\Component\Promotion\Model\PromotionAction as BasePromotionAction;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="sylius_promotion_action")
+ * @Gedmo\Loggable
  */
 class PromotionAction extends BasePromotionAction
 {
+    /**
+     * @var array
+     * @Gedmo\Versioned
+     */
+    protected $configuration = [];
 }
