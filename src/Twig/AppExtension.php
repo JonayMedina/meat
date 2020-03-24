ab4dbcf7b2c1f27f4d66a14f7d83907767cf5f6b
Robinson Roca <rroca@praga.ws>
2020-03-24 17:22:16 -0600

    New functions in Twig

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,8 +2,12 @@
 
 namespace App\Twig;
 
+use Exception;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Twig\TwigFilter;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use Twig_Function;
 
 /**
  * Class AppExtension
@@ -12,10 +16,29 @@ use Twig\Extension\AbstractExtension;
  */
 class AppExtension extends AbstractExtension
 {
+    /** @var ContainerInterface $container */
+    private $container;
+
+    public function __construct(ContainerInterface $serviceContainer)
+    {
+        $this->container = $serviceContainer;
+    }
+
     public function getFilters()
     {
         return [
             new TwigFilter('price', [$this, 'formatPrice']),
+            new TwigFilter('base64', [$this, 'imageToBase64'])
+        ];
+    }
+
+    /**
+     * @return array|Twig_Function[]
+     */
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('getUrl', [$this, 'getUrl']),
         ];
     }
 
@@ -25,5 +48,29 @@ class AppExtension extends AbstractExtension
         $price = number_format($price, $decimals, $decPoint, $thousandsSep);
 
         return $price;
+    }
+
+    public function imageToBase64($url) {
+        $arrContextOptions=array(
+            "ssl"=>array(
+                "verify_peer"=>false,
+                "verify_peer_name"=>false,
+            ),
+        );
+
+        $type = pathinfo($url, PATHINFO_EXTENSION);
+        $data = file_get_contents($url, false, stream_context_create($arrContextOptions));
+
+        return 'data:image/' . $type . ';base64,' . base64_encode($data);
+    }
+
+    /**
+     * Return Main URL.
+     * @throws Exception
+     * @return string
+     */
+    public function getUrl()
+    {
+        return getenv('APP_URL');
     }
 }
