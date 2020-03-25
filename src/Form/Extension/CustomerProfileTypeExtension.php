079d215ef8a0c2c4c2e40f3592080ce325240f75
Robinson Roca <rroca@praga.ws>
2020-03-25 13:48:11 -0600

    Non-defitnitve chagnes

--- src/Form/Extension/CustomerProfileTypeExtension.php
+++ src/Form/Extension/CustomerProfileTypeExtension.php
@@ -5,18 +5,21 @@ namespace App\Form\Extension;
 
 
 use Sylius\Bundle\AddressingBundle\Form\Type\AddressType;
-use Sylius\Bundle\CustomerBundle\Form\Type\CustomerProfileType;
+use Sylius\Bundle\AdminApiBundle\Form\Type\CustomerProfileType;
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\IsTrue;
 
 class CustomerProfileTypeExtension extends AbstractTypeExtension
 {
-
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options): void {
         $builder
-            // Adding new fields works just like in the parent form type.
-            ->add('address', AddressType::class);
+            ->add('termsAccepted', CheckboxType::class, array(
+                'label' => 'app.ui.accept_the',
+                'mapped' => false,
+                'constraints' => new IsTrue()
+            ));
     }
 
     public static function getExtendedTypes(): iterable
