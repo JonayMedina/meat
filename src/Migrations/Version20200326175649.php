6090f95d4a8a5018e4405ad0555681922dfe90a5
Rodmar Zavala <rodmar.zavala@gmail.com>
2020-03-26 12:17:13 -0600

    Links to platform are working now!

---
+++ src/Migrations/Version20200326175649.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200326175649 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE app_about_store ADD facebook_url VARCHAR(255) DEFAULT NULL, ADD twitter_url VARCHAR(255) DEFAULT NULL, ADD instagram_url VARCHAR(255) DEFAULT NULL, ADD pinterest_url VARCHAR(255) DEFAULT NULL, ADD app_store_url VARCHAR(255) DEFAULT NULL, ADD play_store_url VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE app_about_store DROP facebook_url, DROP twitter_url, DROP instagram_url, DROP pinterest_url, DROP app_store_url, DROP play_store_url');
+
+    }
+}
