60261d80cdb0da5b149fa20e8cefcba2ac7ebc8a
Robinson Roca <rroca@praga.ws>
2020-03-27 16:52:20 -0600

    Twig Function 'aboutStore()' added

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,6 +2,7 @@
 
 namespace App\Twig;
 
+use App\Service\SettingsService;
 use Exception;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Twig\TwigFilter;
@@ -19,9 +20,13 @@ class AppExtension extends AbstractExtension
     /** @var ContainerInterface $container */
     private $container;
 
-    public function __construct(ContainerInterface $serviceContainer)
+    /** @var SettingsService $settingsService */
+    private $settingsService;
+
+    public function __construct(ContainerInterface $serviceContainer, SettingsService $settingsService)
     {
         $this->container = $serviceContainer;
+        $this->settingsService = $settingsService;
     }
 
     public function getFilters()
@@ -39,6 +44,7 @@ class AppExtension extends AbstractExtension
     {
         return [
             new TwigFunction('getUrl', [$this, 'getUrl']),
+            new TwigFunction('aboutStore', [$this, 'AboutStore'])
         ];
     }
 
@@ -72,5 +78,32 @@ class AppExtension extends AbstractExtension
     public function getUrl()
     {
         return getenv('APP_URL');
+    }
+
+    /**
+     * @param string $option
+     * @return string|null
+     */
+    public function aboutStore($option = 'about-us') {
+        switch ($option) {
+            case 'facebook': return $this->settingsService->getFacebookUrl();
+            case 'instagram': return $this->settingsService->getInstagramUrl();
+            case 'twitter': return $this->settingsService->getTwitterUrl();
+            case 'pinterest': return $this->settingsService->getPinterestUrl();
+            case 'app-store': return $this->settingsService->getAppStoreUrl();
+            case 'play-store': return $this->settingsService->getPlayStoreUrl();
+            case 'phrase': return $this->settingsService->getPhrase();
+            case 'author': return $this->settingsService->getAuthor();
+            case 'about-us': return $this->settingsService->getAboutUs();
+            case 'delivery-hours': return $this->settingsService->getDeliveryHours();
+            case 'show-search': return $this->settingsService->getShowProductSearchBox();
+            case 'days-to-choose': return $this->settingsService->getDaysToChooseInAdvanceToPurchase();
+            case 'first-purchase-ms': return $this->settingsService->getFirstPurchaseMessage();
+            case 'new-address-ms': return $this->settingsService->getNewAddressMessage();
+            case 'max-purchase': return $this->settingsService->getMaximumPurchaseValue();
+            case 'min-purchase': return $this->settingsService->getMinimumPurchaseValue();
+            default:
+                return $this->settingsService->getAboutUs();
+        }
     }
 }
