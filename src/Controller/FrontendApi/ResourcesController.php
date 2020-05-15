b0b680d78e2fc08438e7dda8f2b613478b1f3e17
Robinson Roca <rroca@praga.ws>
2020-05-14 23:51:37 -0600

    Old cart flow (Frontend)

--- src/Controller/FrontendApi/ResourcesController.php
+++ src/Controller/FrontendApi/ResourcesController.php
@@ -4,6 +4,7 @@ namespace App\Controller\FrontendApi;
 
 use App\Controller\ShopApi\ContactUsController;
 use App\Entity\Product\Product;
+use App\Entity\Promotion\Promotion;
 use App\Entity\User\ShopUser;
 use App\Model\APIResponse;
 use App\Service\CaptchaVerificationService;
@@ -12,6 +13,7 @@ use App\Service\SettingsService;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use Sylius\Component\Mailer\Sender\SenderInterface;
 use Sylius\Component\Product\Repository\ProductRepositoryInterface;
+use Sylius\Component\Promotion\Repository\PromotionRepositoryInterface;
 use Sylius\Component\User\Repository\UserRepositoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -61,6 +63,11 @@ class ResourcesController extends AbstractFOSRestController
     private $userRepository;
 
     /**
+     * @var PromotionRepositoryInterface
+     */
+    private $promotionRepository;
+
+    /**
      * QueueController constructor.
      * @param SenderInterface $sender
      * @param TranslatorInterface $translator
@@ -71,7 +78,7 @@ class ResourcesController extends AbstractFOSRestController
      * @param ContactUsController $contactUsController
      * @param UserRepositoryInterface $userRepository
      */
-    public function __construct(SenderInterface $sender, TranslatorInterface $translator, CaptchaVerificationService $captchaVerification, SettingsService $settingsService, FavoriteService $favoriteService, ProductRepositoryInterface $productRepository, ContactUsController $contactUsController, UserRepositoryInterface $userRepository)
+    public function __construct(SenderInterface $sender, TranslatorInterface $translator, CaptchaVerificationService $captchaVerification, SettingsService $settingsService, FavoriteService $favoriteService, ProductRepositoryInterface $productRepository, ContactUsController $contactUsController, UserRepositoryInterface $userRepository, PromotionRepositoryInterface $promotionRepository)
     {
         $this->sender = $sender;
         $this->translator = $translator;
@@ -81,6 +88,7 @@ class ResourcesController extends AbstractFOSRestController
         $this->productRepository = $productRepository;
         $this->contactUsController = $contactUsController;
         $this->userRepository = $userRepository;
+        $this->promotionRepository = $promotionRepository;
     }
 
     /**
@@ -287,6 +295,52 @@ class ResourcesController extends AbstractFOSRestController
             $data = new APIResponse($statusCode, APIResponse::TYPE_ERROR, 'Error', [
                 'title' => $this->translator->trans('app.ui.reset_password.error.title'),
                 'message' => $this->translator->trans('app.ui.reset_password.error.enter_email')
+            ]);
+        }
+
+        $view = $this->view($data, $statusCode);
+
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Route(
+     *     "/promotion/{code}.{_format}",
+     *     name="store_api_show_promotion",
+     *     methods={"POST"},
+     *     options={"expose" = true}
+     * )
+     * @param Request $request
+     * @param String $code
+     * @return Response
+     */
+    public function showPromotionAction(Request $request, String $code) {
+        /**
+         * @var Promotion
+         */
+        $promotion = $this->promotionRepository->findOneBy(['code' => $code]);
+
+        if ($promotion instanceof Promotion) {
+            if ($user->getPasswordResetToken()) {
+                $this->sender->send('reset_password_token', [$email], ['user' => $user]);
+
+                $statusCode = Response::HTTP_CREATED;
+                $data = new APIResponse($statusCode, APIResponse::TYPE_INFO, 'Ok', [
+                    'title' => $this->translator->trans('app.ui.reset_password.success'),
+                    'message' => $this->translator->trans('app.ui.reset_password.success.message')
+                ]);
+            } else {
+                $statusCode = Response::HTTP_BAD_REQUEST;
+                $data = new APIResponse($statusCode, APIResponse::TYPE_ERROR, 'Error', [
+                    'title' => $this->translator->trans('app.ui.reset_password.error.title'),
+                    'message' => $this->translator->trans('app.ui.reset_password.error.message')
+                ]);
+            }
+        } else {
+            $statusCode = Response::HTTP_BAD_REQUEST;
+            $data = new APIResponse($statusCode, APIResponse::TYPE_ERROR, 'Error', [
+                'title' => $this->translator->trans('app.ui.reset_password.error.title'),
+                'message' => $this->translator->trans('app.ui.reset_password.error.not_registered')
             ]);
         }
 
