642b77cb7726498748e4044d0e9ba8c5d59c6d08
Rodmar Zavala <rodmar.zavala@gmail.com>
2020-05-18 22:54:30 -0600

    Merge carts in /carts index method

--- src/Controller/ShopApi/CartController.php
+++ src/Controller/ShopApi/CartController.php
@@ -2,10 +2,16 @@
 
 namespace App\Controller\ShopApi;
 
+use App\Entity\Order\OrderItem;
+use App\Entity\Product\ProductVariant;
 use App\Model\APIResponse;
 use App\Entity\Order\Order;
 use App\Entity\User\ShopUser;
 use App\Entity\Promotion\PromotionCoupon;
+use Doctrine\ORM\EntityManagerInterface;
+use Sylius\Component\Core\Cart\Modifier\LimitingOrderItemQuantityModifier;
+use Sylius\Component\Core\Factory\CartItemFactoryInterface;
+use Sylius\Component\Order\Processor\CompositeOrderProcessor;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Sylius\Component\Core\Model\OrderInterface;
@@ -18,7 +24,7 @@ use Sylius\Bundle\PromotionBundle\Doctrine\ORM\PromotionCouponRepository;
 
 /**
  * CartController
- * @Route("/sync-carts")
+ * @Route("/carts")
  */
 class CartController extends AbstractFOSRestController
 {
@@ -37,64 +43,127 @@ class CartController extends AbstractFOSRestController
     private $translator;
 
     /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * @var OrderRepository
+     */
+    private $orderRepository;
+
+    /**
+     * @var CartItemFactoryInterface
+     */
+    private $orderItemFactory;
+
+    /**
+     * @var LimitingOrderItemQuantityModifier
+     */
+    private $itemQuantityModifier;
+
+    /**
+     * @var CompositeOrderProcessor
+     */
+    private $compositeOrderProcessor;
+
+    /**
      * CartController constructor.
      * @param OrderRepository $repository
      * @param PromotionCouponRepository $couponRepository
      * @param AddCouponAction $addCouponAction
      * @param TranslatorInterface $translator
+     * @param EntityManagerInterface $entityManager
+     * @param OrderRepository $orderRepository
+     * @param CartItemFactoryInterface $cartItemFactory
+     * @param LimitingOrderItemQuantityModifier $itemQuantityModifier
+     * @param CompositeOrderProcessor $compositeOrderProcessor
      */
-    public function __construct(OrderRepository $repository, PromotionCouponRepository $couponRepository, AddCouponAction $addCouponAction, TranslatorInterface $translator)
+    public function __construct(OrderRepository $repository, PromotionCouponRepository $couponRepository, AddCouponAction $addCouponAction, TranslatorInterface $translator, EntityManagerInterface $entityManager, OrderRepository $orderRepository, CartItemFactoryInterface $cartItemFactory, LimitingOrderItemQuantityModifier $itemQuantityModifier, CompositeOrderProcessor $compositeOrderProcessor)
     {
         $this->repository = $repository;
         $this->couponRepository = $couponRepository;
         $this->addCouponAction = $addCouponAction;
         $this->translator = $translator;
+        $this->entityManager = $entityManager;
+        $this->orderRepository = $orderRepository;
+        $this->orderItemFactory = $cartItemFactory;
+        $this->itemQuantityModifier = $itemQuantityModifier;
+        $this->compositeOrderProcessor = $compositeOrderProcessor;
     }
 
     /**
      * @Route(
      *     ".{_format}",
-     *     name="shop_api_get_carts",
-     *     methods={"GET"}
+     *     name="shop_api_get_cart",
+     *     methods={"POST"}
      * )
      *
      * @return Response
      */
     public function indexAction()
     {
-        $list = [];
-        $statusCode = Response::HTTP_OK;
+        $orders = $this->getOrders();
+        /** @var ProductVariant[] $variants */
+        $variants = [];
+        $mainOrder = $orders[0] ?? null;
 
-        /** @var ShopUser $user */
-        $user = $this->getUser();
+        foreach ($orders as $index => $order) {
+            /** No sumar los productos del carrito que no se eliminará... */
+            if ($index > 0) {
+                /** @var OrderItem $orderItem */
+                foreach ($order->getItems() as $orderItem) {
+                    $variant = $orderItem->getVariant();
+                    $variants[$variant->getId()]['variant'] = $variant;
 
-        if ($user instanceof ShopUser) {
-            /** @var Order[] $orders */
-            $orders = $this->repository
-                ->createQueryBuilder('o')
-                ->andWhere('o.customer = :customer')
-                ->andWhere('o.tokenValue IS NOT NULL')
-                ->andWhere('o.state = :state')
-                ->setParameter('customer', $user->getCustomer())
-                ->setParameter('state', OrderInterface::STATE_CART)
-                ->getQuery()
-                ->getResult();
-
-            foreach ($orders as $order) {
-                $list[] = [
-                    'id' => $order->getId(),
-                    'number' => $order->getNumber(),
-                    'token_value' => $order->getTokenValue(),
-                ];
+                    /** calculate quantity max always... */
+                    if ($orderItem->getQuantity() > ($variants[$variant->getId()]['quantity'] ?? 0)) {
+                        $variants[$variant->getId()]['quantity'] = $orderItem->getQuantity();
+                    }
+                }
             }
-        } else {
-            $list = [];
         }
 
-        $response = new APIResponse($statusCode, APIResponse::TYPE_INFO, 'Cart list', $list);
-        $view = $this->view($response, $statusCode);
+        if ($mainOrder instanceof Order) {
+            foreach ($variants as $variant) {
+                /** @var OrderItem $orderItem */
+                $orderItem = $this->orderItemFactory->createNew();
+                $orderItem->setVariant($variant['variant']);
+                $this->itemQuantityModifier->modify($orderItem, $variant['quantity']);
 
-        return $this->handleView($view);
+                $this->entityManager->persist($orderItem);
+
+                $mainOrder->addItem($orderItem);
+                $this->compositeOrderProcessor->process($mainOrder);
+            }
+        }
+
+        $this->orderRepository->add($mainOrder);
+
+        /** Remove previous carts */
+        foreach ($orders as $index => $order) {
+            if ($index > 0) {
+                $this->orderRepository->remove($order);
+            }
+        }
+
+        try  {
+            $this->entityManager->flush();
+
+            $statusCode = Response::HTTP_OK;
+            $serialized = $this->serializeOrder($mainOrder);
+            $response = new APIResponse($statusCode, APIResponse::TYPE_INFO, 'Merged...', $serialized);
+            $view = $this->view($response, $statusCode);
+
+            return $this->handleView($view);
+
+        } catch (\Exception $exception) {
+            $statusCode = Response::HTTP_NOT_FOUND;
+            $response = new APIResponse($statusCode, APIResponse::TYPE_ERROR, $this->translator->trans('app.api.cart.coupon_not_found'));
+            $view = $this->view($response, $statusCode);
+
+            return $this->handleView($view);
+        }
     }
 
     /**
@@ -132,5 +201,45 @@ class CartController extends AbstractFOSRestController
         $view = $this->view($response, $statusCode);
 
         return $this->handleView($view);
+    }
+
+    /**
+     * Return customer's orders.
+     * @return Order[]
+     */
+    private function getOrders()
+    {
+        /** @var ShopUser $user */
+        $user = $this->getUser();
+
+        /** @var Order[] $orders */
+        $orders = $this->repository
+            ->createQueryBuilder('o')
+            ->andWhere('o.customer = :customer')
+            ->andWhere('o.tokenValue IS NOT NULL')
+            ->andWhere('o.state = :state')
+            ->setParameter('customer', $user->getCustomer())
+            ->setParameter('state', OrderInterface::STATE_CART)
+            ->orderBy('o.createdAt', 'DESC')
+            ->getQuery()
+            ->getResult();
+
+        return $orders;
+    }
+
+    /**
+     * Return serialized order object.
+     * @param Order|null $order
+     * @return array
+     */
+    private function serializeOrder(?Order $order)
+    {
+        return [
+            'id' => $order->getId(),
+            'number' => $order->getNumber(),
+            'items_total' => $order->getTotalQuantity(),
+            'token_value' => $order->getTokenValue(),
+            'created_at' => $order->getCreatedAt()->format('c'),
+        ];
     }
 }
