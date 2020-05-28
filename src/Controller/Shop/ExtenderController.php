d41d2cf6336159bc5e6c14db2422d87b2afd2740
Robinson Roca <rroca@praga.ws>
2020-05-28 15:50:47 -0600

    Change email flow added, with responsive

--- src/Controller/Shop/ExtenderController.php
+++ src/Controller/Shop/ExtenderController.php
@@ -5,16 +5,32 @@ namespace App\Controller\Shop;
 use App\Entity\User\ShopUser;
 use App\Entity\Customer\Customer;
 use App\Entity\Addressing\Address;
+use App\Form\Shop\ChangeEmailType;
 use App\Form\Shop\BillingProfileType;
 use App\Repository\FavoriteRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sylius\Component\Mailer\Sender\SenderInterface;
+use Sylius\Component\User\Repository\UserRepositoryInterface;
 use Sylius\Component\Core\Repository\AddressRepositoryInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sylius\Component\Resource\Generator\RandomnessGeneratorInterface;
 
 class ExtenderController extends AbstractController
 {
+    /** @var UserRepositoryInterface */
+    private $userRepository;
+
+    /**
+     * ExtenderController constructor.
+     * @param UserRepositoryInterface $userRepository
+     */
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     /**
      * @Route("/account/favorites", name="user_favorites")
      * @param FavoriteRepository $favoriteRepository
@@ -70,5 +86,62 @@ class ExtenderController extends AbstractController
         }
 
         return $this->render('shop/account/updateBilling.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/account/change-email", name="user_change_email")
+     * @param Request $request
+     * @param RandomnessGeneratorInterface $generator
+     * @param SenderInterface $sender
+     * @return Response
+     */
+    public function changeEmailAction(Request $request, RandomnessGeneratorInterface $generator, SenderInterface $sender) {
+        $errors = [];
+        $em = $this->getDoctrine()->getManager();
+        /** @var ShopUser $user */
+        $user = $this->getUser();
+
+        $form = $this->createForm(ChangeEmailType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $errors = $this->validateEmails($form->getData());
+
+            if (count($errors) <= 0) {
+                $emails = $form->getData();
+                $user->setTempEmail($emails['newEmail']);
+                $user->setEmailVerificationToken($generator->generateNumeric(10));
+
+                $em->flush();
+
+                $sender->send('change_email_verification', [$emails['email']], ['user' => $user]);
+
+                return $this->redirectToRoute('user_change_email', ['success' => true]);
+            }
+        }
+
+        return $this->render('shop/account/changeEmail.html.twig', ['user' => $user, 'form' => $form->createView(), 'errors' => $errors]);
+    }
+
+    /**
+     * @param $emails
+     * @return array
+     */
+    private function validateEmails($emails) {
+        $errors = [];
+        /** @var ShopUser $user */
+        $user = $this->getUser();
+
+        $existUser = $this->userRepository->findOneBy(['username' => $emails['newEmail']]);
+
+        if ($emails['email'] != $user->getEmail()) {
+            $errors['email'] = 'app.ui.change_email.error.incorrect_actual_email';
+        } else if ($emails['email'] == $emails['newEmail']) {
+            $errors['email'] = 'app.ui.change_email.error.same_email';
+        } else if ($existUser instanceof ShopUser) {
+            $errors['email'] = 'app.ui.change_email.error.email_is_used';
+        }
+
+        return $errors;
     }
 }
---
+++ src/Form/Shop/ChangeEmailType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form\Shop;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ChangeEmailType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'app.ui.form.current_email',
+                'constraints' => [
+                    new NotBlank(),
+                    new Email()
+                ]
+            ])
+            ->add('newEmail', EmailType::class, [
+                'label' => 'app.ui.form.new_email',
+                'constraints' => [
+                    new NotBlank(),
+                    new Email()
+                ]
+            ])
+        ;
+    }
+}
