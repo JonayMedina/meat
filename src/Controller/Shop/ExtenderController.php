eb0879edeba652b68e8e32f2d94462f3913ad7f0
Robinson Roca <rroca@praga.ws>
2020-05-27 19:42:50 -0600

    Change password flow added

--- src/Controller/Shop/ExtenderController.php
+++ src/Controller/Shop/ExtenderController.php
@@ -4,6 +4,7 @@ namespace App\Controller\Shop;
 
 use App\Entity\User\ShopUser;
 use App\Repository\FavoriteRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ExtenderController extends AbstractController
 {
     /**
-     * @Route("/favorites", name="user_favorites")
+     * @Route("/account/favorites", name="user_favorites")
      * @param FavoriteRepository $favoriteRepository
      * @return Response
      */
@@ -23,5 +24,14 @@ class ExtenderController extends AbstractController
         $user = $this->getUser();
 
         return $this->render('shop/account/favorites.html.twig', ['favorites' => $favoriteRepository->findBy(['shopUser' => $user])]);
+    }
+
+    /**
+     * @Route("/account/changed-password", name="user_changed_password")
+     * @param Request $request
+     * @return Response
+     */
+    public function changedPassword(Request $request) {
+        return $this->redirectToRoute('sylius_shop_account_change_password', ['success' => true]);
     }
 }
--- themes/PWSTheme/SyliusShopBundle/public/js/app.js
+++ themes/PWSTheme/SyliusShopBundle/public/js/app.js
@@ -4,6 +4,16 @@ function unblockPage() {
   $.unblockUI();
 }
 
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, '\\$&');
+  var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'), results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+
+  return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
+
 function autocomplete(inp, arr) {
   var currentFocus;
   var a, b, i, val = inp.value;
