fb873c9e06dbe07155c35c506cdfbfbce653247c
Rodmar Zavala <rzavala@praga.ws>
2020-08-07 13:09:07 -0600

    Bugfix

--- src/Controller/Admin/TermsAndConditionsController.php
+++ src/Controller/Admin/TermsAndConditionsController.php
@@ -9,6 +9,7 @@ use App\Form\Admin\TermsAndConditionsType;
 use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\TermsAndConditionsRepository;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Sylius\Bundle\CoreBundle\Doctrine\ORM\CustomerRepository;
