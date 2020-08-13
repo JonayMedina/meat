0020ed7c0e44b98d5be7d5f6772bd2eeeda6ca25
Robinson Roca <rroca@praga.ws>
2020-08-13 16:28:47 -0600

    New changes in register page

--- src/Form/Extension/ShopUserRegistrationTypeExtension.php
+++ src/Form/Extension/ShopUserRegistrationTypeExtension.php
@@ -3,6 +3,8 @@
 namespace App\Form\Extension;
 
 use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Sylius\Bundle\CoreBundle\Form\Type\User\ShopUserRegistrationType;
 
@@ -12,6 +14,16 @@ use Sylius\Bundle\CoreBundle\Form\Type\User\ShopUserRegistrationType;
  */
 class ShopUserRegistrationTypeExtension extends AbstractTypeExtension
 {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->remove('plainPassword')
+            // Adding new fields works just like in the parent form type.
+            ->add('plainPassword', PasswordType::class, [
+                'label' => 'sylius.form.user.password.label'
+            ]);
+    }
+
     /**
      * @param OptionsResolver $resolver
      */
