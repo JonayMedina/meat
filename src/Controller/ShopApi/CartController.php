f60d74f746343c1e54dc97cee16ee0e3a45c9136
Rodmar Zavala <rzavala@praga.ws>
2021-01-20 14:51:52 -0600

    Recalculate again V2

--- src/Controller/ShopApi/CartController.php
+++ src/Controller/ShopApi/CartController.php
@@ -535,10 +535,6 @@ class CartController extends AbstractFOSRestController
 
         $this->addAdjustments($order);
 
-        /** do re-calculation here */
-        $order->recalculateItemsTotal();
-        $order->recalculateAdjustmentsTotal();
-
         $this->entityManager->flush();
         $serialized = $this->orderService->serializeOrder($order);
 
@@ -590,6 +586,10 @@ class CartController extends AbstractFOSRestController
          * @var ShippingMethod $shippingMethod
          */
         $this->addShipping($order);
+
+        /** do re-calculation here */
+        $order->recalculateItemsTotal();
+        $order->recalculateAdjustmentsTotal();
 
         $this->entityManager->flush();
     }
