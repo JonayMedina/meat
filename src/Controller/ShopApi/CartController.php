3a8cd5e74a3ee5d935909501d4971cf4da63388e
Rodmar Zavala <rzavala@praga.ws>
2021-04-29 16:49:29 -0600

    Revert "All timezones to Guatemala V4"
    This reverts commit 9c8a8617

--- src/Controller/ShopApi/CartController.php
+++ src/Controller/ShopApi/CartController.php
@@ -540,7 +540,7 @@ class CartController extends AbstractFOSRestController
 
         $response = new APIResponse($statusCode, APIResponse::TYPE_INFO, 'Ok', [
             'token' => $order->getTokenValue(),
-            'estimated_delivery_date' => $order->getEstimatedDeliveryDate()->modify('+6 hours'),
+            'estimated_delivery_date' => $order->getEstimatedDeliveryDate()->modify('-6 hours')->setTimezone(new \DateTimeZone('UTC')),
             'order' => $this->orderService->serializeOrder($order)
         ]);
 
